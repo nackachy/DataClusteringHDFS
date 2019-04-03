aad463c0034dcdda6a1d787edf83b127e3567ebc
nackachy <30370027+nackachy@users.noreply.github.com>
2019-04-03 15:37:30 +0100

    version.0.1

--- src/main/scala/com/DataVectis/Clustering/Properties.scala
+++ src/main/scala/com/DataVectis/Clustering/Properties.scala
@@ -7,7 +7,7 @@ class prop {
 
   def getProp (propertyName: String) : String  = {
     val properties = new Properties()
-    properties.load(new FileInputStream("src/main/ressources/application.properties"))
+    properties.load(new FileInputStream("/home/chiheb/DataClusteringHDFS/src/main/ressources/application.properties"))
     val property = properties.getProperty(propertyName)
     property.toString()
 
